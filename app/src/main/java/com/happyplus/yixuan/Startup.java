492212be42b340512f0d71d1a7afade44a2f68ce
happyplus <894860970@qq.com>
2022-02-10 02:23:25 +0800

    feat: 开机自启

--- app/src/main/java/com/happyplus/yixuan/Startup.java
+++ app/src/main/java/com/happyplus/yixuan/Startup.java
@@ -13,13 +13,12 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Log.e("HAPPYPLUS", "收到广播正在准备开机自启");
-            Toast.makeText(context, "正在准备打开嘟嘟车机", Toast.LENGTH_LONG);
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    Log.e("HAPPYPLUS", "启动嘟嘟车机");
+                    Log.e("HAPPYPLUS", "开机自启");
                     PackageManager packageManager = context.getPackageManager();
-                    Intent carIntent = packageManager.getLaunchIntentForPackage("com.wow.carlauncher");
+                    Intent carIntent = packageManager.getLaunchIntentForPackage("com.happyplus.yixuan");
                     carIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(carIntent);
                 }
